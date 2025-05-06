8b3e0fea9052e6be73fa783637d1d03046516923
jaiswalpratik389 <jaiswalpratik389@gmail.com>
2025-05-06 12:56:51 +0530

    no change

--- src/component/decorations/index.js
+++ src/component/decorations/index.js
@@ -63,9 +63,12 @@ export const Decorations = () => {
             <div className='decorations-content'>
                 <div className='decorations-content-list'>
                     {decorationData?.map((ele) => (
-                        <div className='decorations-content-img' onClick={() => handleModel(ele)}>
-                            <img src={`${process.env.REACT_APP_BASE_URL}uploads/${ele?.image}`} />
+                        <div className='decorations-content-img'>
+                            <div className='decoration-img-align' onClick={() => handleModel(ele)}>
+                                <img src={`${process.env.REACT_APP_BASE_URL}uploads/${ele?.image}`} />
+                            </div>
                             <div className='decorations-name'>{ele?.category}</div>
+
                         </div>
                     ))}
                 </div>
